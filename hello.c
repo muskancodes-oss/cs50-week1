373d62bb61753d400cdeeddabb4ca507c87706b8
Muskan <muskandahiya23072007@gmail.com>
2026-02-09 03:24:13 -0800

    Add hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name? ");
+    printf("hello, %s\n", name);
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name? ");
+    printf("hello, %s\n", name);
+}
